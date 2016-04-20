539e30319586634b55bbf394bda60165880be589
tgaff <tgaff@alumni.nd.edu>
2016-04-20 15:46:00 -0700

    created router, moved index to template file

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
-angular.module('movieApp', []);
-      //  .config(config)
+angular.module('movieApp', ['ngRoute'])
+        .config(config);
 
 
 
@@ -7,21 +7,21 @@ angular.module('movieApp', []);
 // ROUTES //    //  SAVE THIS FOR LATER
 ////////////
 
-// config.$inject = ['$routeProvider', '$locationProvider'];
-// function config (  $routeProvider,   $locationProvider  )  {
-//   $routeProvider
-//     .when('/', {
-//       templateUrl: '',
-//       controller: '',
-//       controllerAs: ''
-//     })
-//     .otherwise({
-//       redirectTo: '/'
-//     });
-//
-//   $locationProvider
-//     .html5Mode({
-//       enabled: true,
-//       requireBase: false
-//     });
-// }
+config.$inject = ['$routeProvider', '$locationProvider'];
+function config (  $routeProvider,   $locationProvider  )  {
+  $routeProvider
+    .when('/movies', {
+      templateUrl: '/templates/movies-index.template.html',
+      controller: 'MoviesIndexController',
+      controllerAs: 'moviesIndexCtrl'
+    })
+    .otherwise({
+      redirectTo: '/movies'
+    });
+
+  $locationProvider
+    .html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+}
