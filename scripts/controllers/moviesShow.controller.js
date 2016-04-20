59c6ec5d383a79ce2b9223651fd7db8f4523cd4c
tgaff <tgaff@alumni.nd.edu>
2016-04-20 16:34:29 -0700

    setup show route to connect to API

--- scripts/controllers/moviesShow.controller.js
+++ scripts/controllers/moviesShow.controller.js
@@ -4,6 +4,46 @@ angular.module('movieApp')
 MoviesShowController.$inject=['$http', '$routeParams'];
 function MoviesShowController( $http,   $routeParams) {
   var vm = this;
+  var url = 'http://www.omdbapi.com/?i=';
+  var movieId = $routeParams.imdbID;
+  vm.movie = {};
   console.log('hi from the movies show controller');
+
+  //initialization
+  get();
+
+
+  function get() {
+    $http({
+      method: 'GET',
+      url: url + movieId
+    }).then(getSuccessHandler, getErrorHandler);
+  }
+
+  function getSuccessHandler(response) {
+    console.log('response is:', response);
+    vm.movie = response.data;
+  }
+
+  function getErrorHandler() {
+    console.log('something terrible');
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
