f6887d65583599871fbe9a3f73a3a871c6ab2e32
tgaff <tgaff@alumni.nd.edu>
2016-04-20 11:43:37 -0700

    MoviesIndexController.search connected to form

--- scripts/controllers/moviesIndex.controller.js
+++ scripts/controllers/moviesIndex.controller.js
@@ -2,16 +2,18 @@ angular.module('movieApp')
   .controller('MoviesIndexController', MoviesIndexController);
 
 MoviesIndexController.$inject=[];
-function MoviesIndexController( BookService) {
+function MoviesIndexController() {
   var vm = this;
   // exports
   vm.movies = [];
   vm.helloWorld = 'hello world';
+  vm.search = search;
 
   // initialization
   console.log(vm.helloWorld);
 
   // implementations
   function search() {
+    console.log('search called');
   }
 }
